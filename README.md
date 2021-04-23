2faaf8f43b47adf6e583931364194b4a57737549
dudhavix <53200316+dudhavix@users.noreply.github.com>
2021-04-23 19:45:47 -0300

    criação da rota obterJson
    foi criada a rota obterJson do tipo get e post

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# smartScreening
+# Prova prática
+-> No primeiro commit defini a pasta "arquivos" para armazenar o local onde ficaram os jsons
+-> No segundo commit defini a rota "obterJson" com os tipos get e post para demonstrar duas formas de enviar o nome do arquivo a ser lido
--- router.js
+++ router.js
@@ -5,4 +5,16 @@ rotas.get('/', (req, res) => {
     res.send('inicio do projeto')
 })
 
+rotas.get('/obterJson/:nome_arquivo', (req, res) => {
+    var {nome_arquivo} = req.params
+    var arquivo = require(`./arquivos/${nome_arquivo}.json`)
+    res.send(arquivo)
+})
+
+rotas.post('/obterJson', (req, res) => {
+    var {nome_arquivo} = req.body
+    var arquivo = require(`./arquivos/${nome_arquivo}.json`)
+    res.send(arquivo)
+})
+
 module.exports = rotas
